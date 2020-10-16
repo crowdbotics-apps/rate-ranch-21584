89c207d9dee551ab6822531fb2568a82b4be8146
Crowdbotics <team@crowdbotics.com>
2020-10-16 19:41:41 +0000

    Update models

--- backend/rate_ranch_21584/wsgi.py
+++ backend/rate_ranch_21584/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rate_ranch_21584.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rate_ranch_21584.settings")
 
 application = get_wsgi_application()
